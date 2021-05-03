73539df049999657a572e2bf1fc19dda69aca808
Oriol Alàs <oriolalascercos@gmail.com>
2021-05-03 18:40:02 +0200

    p4

--- act2/p4/p4.py
+++ act2/p4/p4.py
@@ -1,4 +1,13 @@
+from __future__ import annotations
 from ply import lex, yacc
+from dataclasses import dataclass
+from typing import List, Optional
+
+@dataclass
+class Estat:
+    index: int
+    simbol: str
+    estats: List[Estat]
 
 
 class Parser:
@@ -7,15 +16,15 @@ class Parser:
         self.lex = lex.lex(module=self)
         self.yacc = yacc.yacc(module=self)
         self.num_line = 1
+        self.num_estats = 0
+        self.estats = dict()
 
-    tokens = ('SYMBOL', 'LAMBDA', 'COMMENT')
+    tokens = ('SYMBOL',)
 
     literals = ['|', '.', '*', '+', '?', '(', ')', ';']
 
     t_SYMBOL = r'[a-d]'
-    t_LAMBDA = r'(?i)buida'
-    t_COMMENT = r'\#.*'
-    t_ignore = ' \t'
+    t_ignore = ' \t\n'
 
     precedence = (
         ('left', '|'),
@@ -31,6 +40,13 @@ class Parser:
     def t_error(self, t):
         t.lexer.skip(1)
 
+    def p_thompson(self, p):
+        """
+        thompson :   thompson regex
+                        | empty
+        """
+        pass
+
     def p_error(self, p):
         print("Syntax error at line %s." % self.num_line)
 
@@ -38,48 +54,105 @@ class Parser:
         """empty :"""
         pass
 
-    def p_thompson(self, p):
+    def p_regex_expr(self, p):
         """
-        thompson :   thompson regex
-                        | empty
+        regex : expr ';'
         """
+        print(f"Estat numerats del 1 al {self.num_estats + 1}")
+        for num, estat in self.estats.items():
+            next_index = [self.num_estats + 1] if not estat.estats else list(map(lambda e: e.index, estat.estats))
+            print(f"[Estat {estat.index}, Símbol {estat.simbol}] Go to {' or '.join(map(str, next_index))}")
+        self.num_line += 1
+        print(f"Estat inicial: {p[1][0].index}")
+        print(f"Estat final: {self.num_estats + 1}")
+        self.num_estats = 0
+        self.estats = dict()
 
-    def p_regex(self, p):
+    def p_regex_empty(self, p):
         """
-        regex :  empty ';'
-                    | expr ';'
+        regex : empty ';'
         """
         self.num_line += 1
+
+    def p_regex_parent(self, p):
+        """
+        expr : '(' expr ')'
+        """
+        p[0] = p[2]
 
     def p_expr_OR(self, p):
         """
         expr : expr '|' expr
         """
+        self.num_estats += 1
+        initial = Estat(self.num_estats, "LAMBDA", [p[1][0], p[3][0]])
+        self.estats[self.num_estats] = initial
+        self.num_estats += 1
+        final = Estat(self.num_estats, "LAMBDA", None)
+        self.estats[self.num_estats] = final
+        p[1][-1].estats = [final]
+        p[3][-1].estats = [final]
+        p[0] = [initial, final]
+
+
 
     def p_expr_CON(self, p):
         """
         expr : expr '.' expr
         """
+        self.num_estats += 1
+        inter = Estat(self.num_estats, "LAMBDA", [p[3][0]])
+        p[1][-1].estats = [inter]
+        self.estats[self.num_estats] = inter
+        p[0] = p[1] + [inter] + p[3]
 
     def p_expr_Kleene(self, p):
         """
         expr : expr '*'
         """
+        self.num_estats += 1
+        final = Estat(self.num_estats, "LAMBDA", None)
+        self.estats[self.num_estats] = final
+        self.num_estats += 1
+        initial = Estat(self.num_estats, "LAMBDA", [p[1][0], final])
+        self.estats[self.num_estats] = initial
+        p[1][-1].estats = [p[1][0], final]
+        p[0] = [initial, final]
+
     def p_expr_POS(self, p):
         """
         expr : expr '+'
         """
+        self.num_estats += 1
+        final = Estat(self.num_estats, "LAMBDA", None)
+        self.estats[self.num_estats] = final
+        self.num_estats += 1
+        initial = Estat(self.num_estats, "LAMBDA", [p[1][0]])
+        self.estats[self.num_estats] = initial
+        p[1][-1].estats = [p[1][0], final]
+        p[0] = [initial, final]
 
     def p_expr_OPT(self, p):
         """
-        expr: expr '?'
+        expr : expr '?'
         """
+        self.num_estats += 1
+        final = Estat(self.num_estats, "LAMBDA", None)
+        self.estats[self.num_estats] = final
+        self.num_estats += 1
+        initial = Estat(self.num_estats, "LAMBDA", [p[1][0], final])
+        self.estats[self.num_estats] = initial
+        p[1][-1].estats = [final]
+        p[0] = [initial, final]
 
-    def p_expr_use(self, p):
+    def p_expr_symbol(self, p):
         """
-            expr: SYMBOL
+            expr : SYMBOL
         """
-        p[0] = p[1]
+        self.num_estats += 1
+        estat = Estat(self.num_estats, p[1], None)
+        p[0] = [estat]
+        self.estats[self.num_estats] = estat
 
     def run(self):
         while True:
