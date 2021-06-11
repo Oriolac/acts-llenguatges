8012aa22020c447f03ac4bc3e1b51a686efcf01c
Oriol Alàs <oriolalascercos@gmail.com>
2021-06-11 17:08:13 +0200

    act3

--- act3/main.py
+++ act3/main.py
@@ -13,9 +13,10 @@ class Parser:
     op_arit = ( 'SUMA', 'RESTA', 'MULT', 'DIV', 'MOD', 'POW')
     op_logics = ('AND', 'OR', 'XOR')
     op_relacionals = ('NOT', 'EQ', 'NEQ', 'GT', 'LT', 'GE', 'LE')
-    tokens = identificadors + constants + op_arit + op_logics + op_relacionals
+    reserved = ('IF', 'ELSE', 'WHILE')
+    tokens = identificadors + constants + op_arit + op_logics + op_relacionals + reserved
 
-    literals = (';', '=', '(', ')')
+    literals = (';', '=', '(', ')', '{', '}')
 
     t_IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
@@ -40,7 +41,9 @@ class Parser:
     t_LT = r'<'
     t_GE = r'>='
     t_LE = r'<='
-
+    t_IF = r'if'
+    t_ELSE = r'else'
+    t_WHILE = r'while'
     t_ignore = ' \t'
 
     precedence = (
@@ -65,14 +68,51 @@ class Parser:
 
     def p_sentence(self, p):
         """
-        sentence :  empty '\n'
-                    | assignment '\n'
+        sentence :  empty ';'
+                    | asig ';'
         """
         self.num_line += 1
     
     def p_empty(self, p):
         """empty :"""
         pass
+    
+    def p_asig(self, p):
+        """
+        asig : IDENTIFIER '=' expr
+        """
+        print(f'{p[1]} = {p[3]};')
+
+    def p_expr_int_suma(self, p):
+        """
+        expr : expr SUMA expr
+        """
+        tmp = self.add_variable()
+        print(f'{tmp} = {p[1]} SUMA {p[3]}')
+        p[0] = tmp
+
+    def p_expr_int_const(self, p):
+        """
+        expr : INTEGER
+        """
+        p[0] = p[1]
+
+    def p_expr_int_uresta(self, p):
+        """
+        expr :  RESTA expr  %prec URESTA
+        """
+        tmp = self.add_variable()
+        print(f'{tmp} = URESTA {p[2]}')
+        p[0] = f"{tmp}"
+
+    def p_expr_int_usuma(self, p):
+        """
+        expr : SUMA expr %prec USUMA
+        """
+        p[0] = f"{p[2]}"
+
+    def add_variable(self):
+        return "tmp"
 
     def run(self):
         while True:
