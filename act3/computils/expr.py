2e3d35e5b5b8c10c7219ba4454ffbc560640a1d8
Oriol Alàs <oriolalascercos@gmail.com>
2021-06-17 10:59:56 +0200

    check types

--- act3/computils/expr.py
+++ act3/computils/expr.py
@@ -1,38 +1,68 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Optional
 from abc import ABC, abstractmethod
+
 
 @dataclass
 class Expr:
     tipus: Type
     value: Any
 
+
 @dataclass
 class Type(ABC):
-    pass
+    
+    def isInstance(self, obj):
+        return isinstance(obj, self.__class__)
+
 
 @dataclass
 class Boolean(Type):
-    
+
     def getSize(self):
         return 1
 
 @dataclass
-class Integer(Type):
-    
+class Num(Type):
+
+    def isInstance(self, obj):
+        return isinstance(obj, Num)
+
+@dataclass
+class Integer(Num):
+
     def getSize(self):
         return 4
 
+
 @dataclass
-class Float(Type):
-    
+class Float(Num):
+
     def getSize(self):
         return 4
+
 
 @dataclass
 class Char(Type):
-    
+
     def getSize(self):
         return 2
+
+
+class List(Type):
+
+    def __init__(self, elementType: Optional[Type] = None):
+        self.elementType: Optional[Type] = elementType
+
+    def getSize(self, list):
+        return lambda x: x.size()
+    
+    def isInstance(self, obj):
+        if not isinstance(obj, List):
+            return False
+        if isinstance(self.elementType, List):
+            return obj.elementType.isInstance(self.elementType)
+        return self.elementType.isInstance(obj.elementType)
+
--- act3/main.py
+++ act3/main.py
@@ -2,45 +2,51 @@ import re
 from ply import lex, yacc
 import sys
 from computils.stable import *
-from computils.expr import Expr, Type, Boolean, Integer, Float, Char
+from computils.expr import Expr, Type, Boolean, Integer, Float, Char, List
 from computils.exceptions import CompileException
+
 
 class Parser:
 
     identificadors = ('IDENTIFIER',)
     constants = ('INTEGER_VALUE', 'FLOAT_VALUE', 'BOOL_VALUE', 'CHAR_VALUE')
-    op_arit = ( 'SUMA', 'RESTA', 'MULT', 'DIV', 'MOD', 'POW')
+    op_arit = ('SUMA', 'RESTA', 'MULT', 'DIV', 'MOD', 'POW')
     op_logics = ('AND', 'OR', 'XOR', 'NOT')
     op_relacionals = ('EQ', 'NEQ', 'GT', 'LT', 'GE', 'LE')
-    reserved = ('IF', 'ELSE', 'ELIF', 'WHILE', 'FUNCTION', 'RETURN', 'INT_TYPE', 'FLOAT_TYPE', 'CHAR_TYPE', 'BOOL_TYPE', 'FOR', 'USING', 'REPEAT', 'UNTIL')
-    tokens = identificadors + constants + op_arit + op_logics + op_relacionals + reserved
-    literals = (';', '=', '(', ')', '{', '}', ',', ':')   
+    reserved = ('IF', 'ELSE', 'ELIF', 'WHILE', 'FUNCTION', 'RETURN', 'INT_TYPE',
+                'FLOAT_TYPE', 'CHAR_TYPE', 'BOOL_TYPE', 'FOR', 'USING', 'REPEAT', 'UNTIL')
+    tokens = identificadors + constants + op_arit + \
+        op_logics + op_relacionals + reserved
+    literals = (';', '=', '(', ')', '{', '}', ',', ':', '[', ']')
 
     def __init__(self):
         self.lex = lex.lex(module=self)
         self.yacc = yacc.yacc(module=self)
         self.num_line = 1
         self.root_table: SymbolTable = SymbolTable(None, '')
-        self.dict_ops_arit = dict(zip(['+', '-', '*', '/', '%', '**'], self.op_arit))
+        self.dict_ops_arit = dict(
+            zip(['+', '-', '*', '/', '%', '**'], self.op_arit))
         self.dict_ops_bool = dict(zip(['and', 'or', 'xor'], self.op_logics))
-        self.dict_ops_rel = dict(zip([self.t_EQ, self.t_NEQ, self.t_GT, self.t_LT, self.t_GE, self.t_LE], self.op_relacionals))
+        self.dict_ops_rel = dict(zip(
+            [self.t_EQ, self.t_NEQ, self.t_GT, self.t_LT, self.t_GE, self.t_LE], self.op_relacionals))
         self.current_table: SymbolTable = self.root_table
-        self.dict_types = {'int': Integer(), 'float': Float(), 'char': Char(), 'bool': Boolean()}
+        self.dict_types = {'int': Integer(), 'float': Float(),
+                           'char': Char(), 'bool': Boolean()}
         self.num_cond_label = 0
-        self.level_if = 0;
+        self.level_if = 0
 
     t_INTEGER_VALUE = r'\d+'
     t_FLOAT_VALUE = r'\d+\.\d*'
     t_BOOL_VALUE = r'(True|False)'
     t_CHAR_VALUE = r'\'.\''
-    
+
     t_SUMA = r'\+'
     t_RESTA = r'-'
     t_MULT = r'\*'
     t_DIV = r'\/'
     t_MOD = r'%'
     t_POW = r'\*\*'
-    
+
     t_AND = r'and'
     t_OR = r'or'
     t_XOR = r'xor'
@@ -51,7 +57,6 @@ class Parser:
     t_LT = r'<'
     t_GE = r'>='
     t_LE = r'<='
-
 
     t_IF = r'if'
     t_ELSE = r'else'
@@ -69,35 +74,37 @@ class Parser:
     t_UNTIL = r'until'
 
     reserved = {
-        t_IF : 'IF',
-        t_ELSE : 'ELSE',
-        t_ELIF : 'ELIF',
-        t_WHILE : 'WHILE',
-        t_FUNCTION : 'FUNCTION',
-        t_RETURN : 'RETURN',
-        t_INT_TYPE : 'INT_TYPE',
-        t_FLOAT_TYPE : 'FLOAT_TYPE',
-        t_CHAR_TYPE : 'CHAR_TYPE',
-        t_BOOL_TYPE : 'BOOL_TYPE',
-        t_BOOL_VALUE : 'BOOL_VALUE',
-        t_AND : 'AND',
-        t_OR : 'OR',
-        t_XOR : 'XOR',
-        t_NOT : 'NOT',
-        t_FOR : 'FOR',
-        t_USING : 'USING',
-        t_REPEAT : 'REPEAT',
-        t_UNTIL : 'UNTIL'
-    } 
-    
+        t_IF: 'IF',
+        t_ELSE: 'ELSE',
+        t_ELIF: 'ELIF',
+        t_WHILE: 'WHILE',
+        t_FUNCTION: 'FUNCTION',
+        t_RETURN: 'RETURN',
+        t_INT_TYPE: 'INT_TYPE',
+        t_FLOAT_TYPE: 'FLOAT_TYPE',
+        t_CHAR_TYPE: 'CHAR_TYPE',
+        t_BOOL_TYPE: 'BOOL_TYPE',
+        t_BOOL_VALUE: 'BOOL_VALUE',
+        t_AND: 'AND',
+        t_OR: 'OR',
+        t_XOR: 'XOR',
+        t_NOT: 'NOT',
+        t_FOR: 'FOR',
+        t_USING: 'USING',
+        t_REPEAT: 'REPEAT',
+        t_UNTIL: 'UNTIL'
+    }
+
     def t_error(self, t):
         print(f"No sha reconegut token en la línea: {self.num_line}")
         t.lexer.skip(1)
-    
+
     def t_IDENTIFIER(self, t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        matches = list(filter(lambda x: re.match(x, t.value), self.reserved.keys()))
-        t.type = 'IDENTIFIER' if not matches else self.reserved.get(matches[0], 'IDENTIFIER')
+        matches = list(filter(lambda x: re.match(
+            x, t.value), self.reserved.keys()))
+        t.type = 'IDENTIFIER' if not matches else self.reserved.get(
+            matches[0], 'IDENTIFIER')
         return t
 
     t_ignore = ' \t\n'
@@ -114,7 +121,6 @@ class Parser:
         ('left', 'POW'),
         ('right', 'USUMA', 'URESTA'),
     )
-
 
     def p_programa(self, p):
         """
@@ -134,7 +140,7 @@ class Parser:
                     | for
         """
         self.num_line += 1
-    
+
     def p_cond(self, p):
         """
         cond : ifCond elseCond
@@ -163,13 +169,12 @@ class Parser:
                     | empty
         """
 
-
     def p_elseCond_else(self, p):
         """
         elseCond :   ELSE '{' bodyCond '}'
         """
         self.level_if -= 1
-    
+
     def p_elseCond_noop(self, p):
         """
         elseCond : empty
@@ -186,7 +191,7 @@ class Parser:
         etiq = self.add_cond_label()
         print(f"{self.tab()}if false {p[2].value} goto {etiq}")
         p[0] = etiq
-        
+
     def p_headElifCond(self, p):
         """
         headElifCond : elifCond  '{' bodyCond '}'
@@ -231,7 +236,7 @@ class Parser:
         print(f"{self.tab()}Label {etiq}")
         self.level_if += 1
         p[0] = etiq
-    
+
     def p_forBody(self, p):
         """
         forBody : sentence forBody
@@ -281,7 +286,6 @@ class Parser:
         print(f"{self.tab()}if false {p[2].value} goto {p[1][1]}")
         p[0] = p[1]
 
-
     def p_addLabelWhile(self, p):
         """
         addLabelWhile : WHILE
@@ -299,13 +303,12 @@ class Parser:
         if not isinstance(p[1].tipus, Boolean):
             raise CompileException(self, "Expression must be boolean.")
         p[0] = p[1]
-        
+
     def p_footWhile(self, p):
         """
         footWhile : '{' bodyWhile '}' 
         """
 
-        
     def p_bodyWhile(self, p):
         """
         bodyWhile : sentence bodyWhile
@@ -340,7 +343,7 @@ class Parser:
         print(f'{self.tab()}{self.t_FUNCTION} {p[3]}:')
         self.current_table = SymbolTable(self.current_table, p[3])
         p[0] = p[2], p[3]
-        
+
     def p_footing(self, p):
         """
         footing : '{' bodyfunk '}'
@@ -353,7 +356,7 @@ class Parser:
                       | FLOAT_TYPE
                       | BOOL_TYPE
                       | CHAR_TYPE
-        """ 
+        """
         p[0] = p[1]
 
     def p_listfunkparams(self, p):
@@ -365,22 +368,19 @@ class Parser:
             p[0] = [p[1]]
         else:
             p[0] = [p[1]] + p[3]
-            
-    
+
     def p_funkparam(self, p):
         """
         funkparam : typename ':' IDENTIFIER
         """
         self.current_table.put(VariableSymbol(p[3], self.dict_types[p[1]]))
-        p[0] = (p[3] , p[1])
-   
-   
+        p[0] = (p[3], p[1])
+
     def p_bodyfunk(self, p):
         """
         bodyfunk : bodysentence bodyfunk
                    | RETURN returnsentence
         """
-        
 
     def p_bodysentence(self, p):
         """
@@ -398,7 +398,7 @@ class Parser:
         empty :
         """
         pass
-    
+
     def p_asig(self, p):
         """
         asig : memoryVariable '=' expr
@@ -406,7 +406,7 @@ class Parser:
         self.current_table.put(VariableSymbol(p[1], p[3].tipus))
         print(f'{self.tab()}{p[1]} = {p[3].value};')
         p[0] = p[1:]
-    
+
     def p_memoryVariable_identifier(self, p):
         """
         memoryVariable : IDENTIFIER
@@ -434,24 +434,28 @@ class Parser:
         funkcall : IDENTIFIER '(' paramscall ')'
         """
         if not self.current_table.has(p[1]):
-            raise CompileException(self, f"Function {p[1]} is not defined yet.")
-      
+            raise CompileException(
+                self, f"Function {p[1]} is not defined yet.")
+
         funk = self.current_table.get(p[1])
 
         if len(funk.arguments) > len(p[3]):
-            raise CompileException(self, f"Not enough arguments for function {p[1]}")
+            raise CompileException(
+                self, f"Not enough arguments for function {p[1]}")
         elif len(funk.arguments) < len(p[3]):
-            raise CompileException(self, f"Too many arguments for function {p[1]}")
+            raise CompileException(
+                self, f"Too many arguments for function {p[1]}")
 
-        
         for i, expr in enumerate(reversed(p[3])):
             actual_type = expr.tipus
             ident = expr.value
-            expected_type = self.dict_types.get(funk.arguments[len(p[3]) - i -1][1])
+            expected_type = self.dict_types.get(
+                funk.arguments[len(p[3]) - i - 1][1])
             if expected_type != actual_type:
-                raise CompileException(self, f"Argument {ident} type is {actual_type} and should be {expected_type}")
+                raise CompileException(
+                    self, f"Argument {ident} type is {actual_type} and should be {expected_type}")
             print(f'{self.tab()}param {ident}')
-            
+
         print(f'{self.tab()}call {p[1]}')
         p[0] = Expr(funk.type, '$SP')
 
@@ -460,17 +464,44 @@ class Parser:
         funkcall : IDENTIFIER '(' ')'
         """
         if not self.current_table.has(p[1]):
-            raise CompileException(self, f"Function {p[1]} is not defined yet.")
-      
+            raise CompileException(
+                self, f"Function {p[1]} is not defined yet.")
+
         funk = self.current_table.get(p[1])
 
         if len(funk.arguments) != 0:
-            raise CompileException(self, f"Function {p[1]} doesn't accept any argument.")
+            raise CompileException(
+                self, f"Function {p[1]} doesn't accept any argument.")
 
         print(f'{self.tab()}call {p[1]}')
         p[0] = Expr(funk.type, '$SP')
 
-    
+    def p_expr_list(self, p):
+        """
+        expr : '[' listElem ']'
+                | '[' ']'
+        """
+        if len(p[1:]) == 2:
+            tipus = List()
+            tmp = self.add_variable(tipus)
+        else:
+            tipus = List(elementType=p[2][0].tipus)
+            tmp = self.add_variable(tipus)
+        p[0] = Expr(tipus=tipus, value=tmp)
+
+    def p_listElem(self, p):
+        """
+        listElem : expr
+                    | expr ',' listElem
+        """
+        if len(p[1:]) == 1:
+            p[0] = [p[1]]
+        else:
+            if not p[1].tipus.isInstance(p[3][-1].tipus):
+                raise CompileException(self, "List is not the same tipus")
+
+            p[0] = [p[1]] + p[3]
+
     def p_expr_funkcall(self, p):
         """
         expr : funkcall
@@ -485,17 +516,17 @@ class Parser:
                     | expr DIV expr
                     | expr MOD expr
                     | expr POW expr
-        """  
+        """
         if (p[1].tipus == p[3].tipus):
             tmp = self.add_variable(p[1].tipus)
         elif (isinstance(p[1].tipus, (Float, Integer)) and isinstance(p[3].tipus, (Float, Integer))):
             tmp = self.add_variable(Float())
         else:
-            raise CompileException(self, f"Not supported types for operating the operation {p[2]}")
-        print(f'{self.tab()}{tmp} = {p[1].value} {self.dict_ops_arit[p[2]]} {p[3].value};')
+            raise CompileException(
+                self, f"Not supported types for operating the operation {p[2]}")
+        print(
+            f'{self.tab()}{tmp} = {p[1].value} {self.dict_ops_arit[p[2]]} {p[3].value};')
         p[0] = Expr(Integer(), tmp)
-    
-    
 
     def p_expr_boolop(self, p):
         """
@@ -504,9 +535,11 @@ class Parser:
                 | expr XOR expr
         """
         if isinstance(p[1].tipus, Boolean) and p[1].tipus != p[3].tipus:
-            raise CompileException(self, f"Not supported types for operating the operation {p[2]}")
+            raise CompileException(
+                self, f"Not supported types for operating the operation {p[2]}")
         tmp = self.add_variable(Boolean())
-        print(f'{self.tab()}{tmp} = {p[1].value} {self.dict_ops_bool[p[2]]} {p[3].value};')
+        print(
+            f'{self.tab()}{tmp} = {p[1].value} {self.dict_ops_bool[p[2]]} {p[3].value};')
         p[0] = Expr(Boolean(), tmp)
 
     def p_expr_boolcomparison(self, p):
@@ -520,17 +553,20 @@ class Parser:
         """
         if p[1].tipus == p[3].tipus or isinstance(p[1].tipus, (Float, Integer)) and isinstance(p[3].tipus, (Float, Integer)):
             tmp = self.add_variable(Boolean())
-            print(f'{self.tab()}{tmp} = {p[1].value} {self.dict_ops_rel[p[2]]} {p[3].value};')
+            print(
+                f'{self.tab()}{tmp} = {p[1].value} {self.dict_ops_rel[p[2]]} {p[3].value};')
             p[0] = Expr(Boolean(), tmp)
         else:
-            raise CompileException(self, f"Not supported types for operating the operation {p[2]}")
+            raise CompileException(
+                self, f"Not supported types for operating the operation {p[2]}")
 
     def p_expr_bool_not(self, p):
         """
         expr :  NOT expr
         """
         if not isinstance(p[2].tipus, Boolean):
-            raise CompileException(self, f"Not supported types for operating the operation {p[0]}")
+            raise CompileException(
+                self, f"Not supported types for operating the operation {p[0]}")
         tmp = self.add_variable(p[2].tipus)
         print(f'{self.tab()}{tmp} = NOT {p[2].value};')
         p[0] = Expr(Boolean(), tmp)
@@ -540,7 +576,6 @@ class Parser:
         expr :   INTEGER_VALUE
         """
         p[0] = Expr(Integer(), p[1])
-
 
     def p_expr_const_float(self, p):
         """
@@ -575,7 +610,8 @@ class Parser:
         expr :  RESTA expr  %prec URESTA
         """
         if not isinstance(p[2].tipus, (Float, Integer)):
-            raise CompileException(f"RESTA is not allowed for type {p[2].tipus}")
+            raise CompileException(
+                f"RESTA is not allowed for type {p[2].tipus}")
         tmp = self.add_variable(p[2].tipus)
         print(f'{self.tab()}{tmp} = URESTA {p[2].value};')
         p[0] = Expr(p[2].tipus, tmp)
@@ -585,17 +621,18 @@ class Parser:
         expr : SUMA expr %prec USUMA
         """
         if not isinstance(p[2].tipus, (Float, Integer)):
-            raise CompileException(f"SUMA is not allowed for type {p[2].tipus}")
+            raise CompileException(
+                f"SUMA is not allowed for type {p[2].tipus}")
         tmp = self.add_variable(p[2].tipus)
         print(f'{self.tab()}{tmp} = USUMA {p[2].value};')
         p[0] = Expr(p[2].tipus, tmp)
 
     def add_variable(self, tipus: Type):
         name = "tmp" + f"{self.current_table.length()}"
-        if not self.current_table.put(VariableSymbol(name, tipus)) :
+        if not self.current_table.put(VariableSymbol(name, tipus)):
             raise CompileException(self, "State unreachable.")
         return name
-    
+
     def add_cond_label(self):
         self.num_cond_label += 1
         return f"etiq{self.num_cond_label}"
@@ -611,12 +648,13 @@ class Parser:
             input = f.read()
         yacc.parse(input, debug=False)
 
+
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 2:
         print(f"usage: python main.py <file>")
         sys.exit()
-    try :
+    try:
         Parser().run()
     except CompileException as e:
         print(e.get_msg())
